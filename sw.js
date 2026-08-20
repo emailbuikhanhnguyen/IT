a57b69ee2c29d2908fe39aa49c029e8ab0148193
emailbuikhanhnguyen <emailbuikhanhnguyen@gmail.com>
2026-08-20 10:59:13 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="it-asset-v12";const CORE=["./","./index.html","./styles.css","./app.js","./employees.js","./manifest.webmanifest"];
+const CACHE="it-asset-v13";const CORE=["./","./index.html","./styles.css","./app.js","./employees.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener("activate",e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(r.ok){let cp=r.clone();caches.open(CACHE).then(x=>x.put(e.request,cp))}return r}).catch(()=>caches.match("./index.html"))) )});
